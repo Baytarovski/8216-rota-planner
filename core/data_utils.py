db58a095a45b6c06046037bf4f8ffb6c4952b184
Dogukan <dogukandag58@gmail.com>
2025-05-22 20:28:42 +0100

    Update data_utils.py

--- core/data_utils.py
+++ core/data_utils.py
@@ -13,4 +13,8 @@ def save_json(path, data):
         json.dump(data, f, indent=2)
 
 def get_week_start_date(selected_friday):
-    return selected_friday - timedelta(days=4)  # Friday -> Monday
+    # Calculate the next Monday after the selected Friday
+    days_to_next_monday = (7 - selected_friday.weekday() + 0) % 7
+    if days_to_next_monday == 0:
+        days_to_next_monday = 7  # If Friday is selected (weekday = 4), then +3 days → Monday
+    return selected_friday + timedelta(days=days_to_next_monday)
