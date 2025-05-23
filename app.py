431f21dba60a8da85fdc199315eb7f8684ac7fca
Dogukan <dogukandag58@gmail.com>
2025-05-24 00:22:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -252,7 +252,9 @@ if is_admin:
     for wk in week_list:
         with st.expander(f"📆 {wk}"):
             rota_data = rotas[wk]
-            rota_df = pd.DataFrame.from_dict(rota_data, orient="index").reindex(columns=["CAR1", "HEAD", "CAR2", "OFFAL", "FCI", "OFFLINE"], fill_value="")
+            rota_df = pd.DataFrame.from_dict(rota_data, orient="index")
+            rota_df = rota_df.reindex(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"])
+            rota_df = rota_df[["CAR1", "HEAD", "CAR2", "OFFAL", "FCI", "OFFLINE"]].fillna("")
             edited_df = st.data_editor(rota_df, key=f"edit_{wk}")
             col1, col2 = st.columns([1,1])
             with col1:
