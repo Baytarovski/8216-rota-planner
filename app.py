60226476225a2121952c87e2c01600854835e782
Dogukan <dogukandag58@gmail.com>
2025-05-25 00:31:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -88,8 +88,7 @@ with st.sidebar.expander("🔐 Admin Access", expanded=False):
     admin_input = st.text_input("Enter admin password:", type="password")
     if admin_input == "1234":
         st.success("Access granted. Admin panel is now visible.")
-    is_admin = True
-    elif admin_input != "":
+        elif admin_input != "":
         st.error("Incorrect password.")
         is_admin = False
     else:
