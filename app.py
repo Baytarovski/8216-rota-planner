2f37c8db04f233c751b742e64dcd7950a7b40369
Dogukan <dogukandag58@gmail.com>
2025-05-24 21:01:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# © 2025 Doğukan Dağ. All rights reserved.
+# This file is protected by copyright law.
+# Unauthorized use, copying, modification, or distribution is strictly prohibited.
+# Contact: ticked.does-7c@icloud.com
+
 # app.py
 # Main Streamlit Application Entry Point
 
