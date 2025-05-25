d89839c2d03a99b22632c6ea7068fdc388cc84e1
Dogukan <dogukandag58@gmail.com>
2025-05-25 15:04:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ if is_admin:
                     st.cache_data.clear()
                     st.rerun()
 
-    st.markdown("<h4 style='margin-top:2em;'>📈 Monthly Summary</h4>", unsafe_allow_html=True)
+    st.markdown("<h4 style='margin-top:0;'>📈 Monthly Summary</h4><hr style='margin-top:0.3em; margin-bottom:1em;'>", unsafe_allow_html=True)
 
     available_months = sorted({datetime.strptime(w, "%Y-%m-%d").strftime("%B %Y") for w in rotas.keys()}, reverse=True)
     selected_month = st.selectbox("📅 Select Month for Summary", available_months)
