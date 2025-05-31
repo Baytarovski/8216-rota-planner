fcabe89476eba60b2e4d6f56c030141eff5c9936
Dogukan <dogukandag58@gmail.com>
2025-05-31 10:02:35 +0100

    Update algorithm.py

--- core/algorithm.py
+++ core/algorithm.py
@@ -25,7 +25,9 @@ def calculate_fairness_scores(rotas, current_week_key, current_week_assignments)
     past_day_count = defaultdict(int)
 
     all_weeks = sorted(rotas.keys(), reverse=True)
-    past_weeks = [w for w in all_weeks if w < current_week_key][:4]
+    current_date = datetime.strptime(current_week_key, "%Y-%m-%d")
+    parsed_weeks = [w for w in all_weeks if datetime.strptime(w, "%Y-%m-%d") < current_date]
+    past_weeks = parsed_weeks[:4]
     st.write("🟦 Weeks included in scoring:", past_weeks)
 
     for week_key in past_weeks:
